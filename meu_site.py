f00b95524eeef05e267fba6cdeb4965ccb755fac
Silvio Cesar Teixeira dos Santos <154469806+silvioctsvix@users.noreply.github.com>
2023-12-21 14:36:27 -0300

    Add files via upload

--- meu_site.py
+++ meu_site.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 # route -> hashtagtreinamentos.com/
 # função -> o que você quer exibir naquela página
 # template
+
+class Requisicao:
+    def __init__(self, code, state):
+        self.code = code
+        self.state = state
 
 @app.route("/")
 def homepage():
@@ -17,17 +22,34 @@ def contatos():
 def usuarios(nome_usuario):
     return render_template("usuarios.html", nome_usuario=nome_usuario)
 
-@app.route("/GetCode")
-def GetCode():
-    requester_url = request.url
-    data = request.json()
-    if 'code' in data and 'state' in data:
-        code = data['code']
-        state = data['state']
-#        return jsonify(response), 200
-# Inclua um script JavaScript na resposta para imprimir no console
-    requester_url = data
-    return render_template("GetCode.html", requester_url=requester_url)
+# @app.route("/GetCode")
+# def GetCode():
+#     requester_url = request.url
+#     data = request.json()
+#     if 'code' in data and 'state' in data:
+#         code = data['code']
+#         state = data['state']
+# #        return jsonify(response), 200
+# # Inclua um script JavaScript na resposta para imprimir no console
+#     requester_url = data
+#     return render_template("GetCode.html", requester_url=requester_url)
+######
+@app.route('/GetCode', methods=['POST'])
+def get_code():
+    requisicao = Requisicao(request.form['code'], request.form['state'])
+    html_response = f"<html><body>Código: {requisicao.code}<br>Estado: {requisicao.state}</body></html>"
+    return html_response, 200, {'Content-Type': 'text/html'}
+ 
+@app.route('/Code', methods=['POST'])
+def code():
+    data = request.get_json()
+    html_response = f"<html><body>Dados recebidos: {data}</body></html>"
+    return html_response, 200, {'Content-Type': 'text/html'}
+ 
+@app.route('/Teste', methods=['GET'])
+def teste():
+    return "<html><body><p>Teste</p></body></html>", 200, {'Content-Type': 'text/html'}
+######
 
 # colocar o site no ar
 if __name__ == "__main__":
