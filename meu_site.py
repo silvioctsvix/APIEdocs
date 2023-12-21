6b6c6afad457ff4132020339b7f369801511432e
Silvio Cesar Teixeira dos Santos <154469806+silvioctsvix@users.noreply.github.com>
2023-12-21 13:01:44 -0300

    Add files via upload

--- meu_site.py
+++ meu_site.py
@@ -20,6 +20,19 @@ def usuarios(nome_usuario):
 @app.route("/GetCode")
 def GetCode():
     requester_url = request.url
+    data = request.json
+    if 'code' in data and 'state' in data:
+        code = data['code']
+        state = data['state']
+ 
+        # Faça o que for necessário com os parâmetros (aqui, apenas devolvendo-os como JSON)
+ #       response = {
+ #           'code': code,
+ #           'state': state
+ #       }
+ 
+#        return jsonify(response), 200
+    requester_url=code
     return render_template("GetCode.html", requester_url=requester_url)
 
 # colocar o site no ar
